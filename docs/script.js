6db01c168249d9a470d6eab47d42762149328c3c
EzeGamer135 <ezesouto2@gmail.com>
2025-02-05 14:49:02 -0300

    Major Update

---
+++ docs/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("Investigaciones/protocolo-phi-crispr.md") // Ojo con la ruta
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar el archivo Markdown.");
+            }
+            return response.text();
+        })
+        .then(text => {
+            document.getElementById("content").innerHTML = marked.parse(text);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            document.getElementById("content").innerHTML = "Error al cargar el archivo.";
+        });
+});
